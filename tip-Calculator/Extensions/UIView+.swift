ffb8f56c1c1d7cb5d15655a723766d56b24d83c1
Felipe Assis <felipeassis97@gmail.com>
2024-03-04 16:55:48 -0300

    feat: input tip view

--- tip-Calculator/Extensions/UIView+.swift
+++ tip-Calculator/Extensions/UIView+.swift
@@ -10,7 +10,7 @@ import UIKit
 extension UIView {
     func addBorderShadow(offset: CGSize, color: UIColor, radius: CGFloat, opacity: Float) {
         layer.cornerRadius = radius
-        layer.masksToBounds = true
+        layer.masksToBounds = false
         layer.shadowOffset = offset
         layer.shadowColor = color.cgColor
         layer.shadowRadius = radius
@@ -18,5 +18,10 @@ extension UIView {
         let backgroundCgColor = backgroundColor?.cgColor
         backgroundColor = nil
         layer.backgroundColor = backgroundCgColor
+    }
+    
+    func addCorderRadius(radius: CGFloat) {
+        layer.masksToBounds = false
+        layer.cornerRadius = radius
     }
 }
--- tip-Calculator/View/BillInputView.swift
+++ tip-Calculator/View/BillInputView.swift
@@ -8,8 +8,106 @@
 import UIKit
 
 class BillInputView: UIView {
-
     
+    private lazy var headerView: HeaderView = {
+        return HeaderView()
+    }()
+    
+    private lazy var textFieldContainerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .white
+        view.addCorderRadius(radius: 8.0)
+        return view
+    }()
+    
+    private lazy var currencyDenominationLabel: UILabel = {
+        let label = UILabel()
+        label.text = "$"
+        label.font = ThemeFont.bold(offSize: 24)
+        label.setContentHuggingPriority(.defaultHigh, for: .horizontal)
+        return label
+    }()
+    
+    private lazy var textField: UITextField = {
+        let field = UITextField()
+        field.borderStyle = .none
+        field.font = ThemeFont.demiBold(offSize: 28)
+        field.keyboardType = .decimalPad
+        field.setContentHuggingPriority(.defaultLow, for: .horizontal)
+        field.tintColor = .onBackground
+        field.textColor = .onBackground
+        //Add toolbar
+        let toolbar = UIToolbar(frame: CGRect(
+            x: 0, y: 0,
+            width: frame.size.width,
+            height: 36))
+        toolbar.barStyle = .default
+        toolbar.sizeToFit()
+        let doneButton = UIBarButtonItem(
+            title: "Done",
+            style: .plain,
+            target: self,
+            action: #selector(doneButtonTapped))
+        toolbar.items = [
+            UIBarButtonItem(
+                barButtonSystemItem: .flexibleSpace,
+                target: nil,
+                action: nil),
+            doneButton
+        ]
+        toolbar.isUserInteractionEnabled = true
+        field.inputAccessoryView = toolbar
+        return field
+    }()
+    
+    @objc private func doneButtonTapped() {}
+    
+    init() {
+        super.init(frame: .zero)
+        layout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func layout() {
+        [headerView, textFieldContainerView].forEach(addSubview(_:))
+        
+        headerView.snp.makeConstraints { make in
+            make.leading.equalToSuperview()
+            make.centerY.equalTo(textFieldContainerView.snp.centerY)
+            make.width.equalTo(68)
+            make.trailing.equalTo(textFieldContainerView.snp.leading).offset(-24)
+        }
+        
+        textFieldContainerView.snp.makeConstraints { make in
+            make.top.trailing.bottom.equalToSuperview()
+        }
+        
+        textFieldContainerView.addSubview(currencyDenominationLabel)
+        textFieldContainerView.addSubview(textField)
+        
+        currencyDenominationLabel.snp.makeConstraints { make in
+            make.top.bottom.equalToSuperview()
+            make.leading.equalTo(textFieldContainerView.snp.leading).offset(16)
+        }
+        
+        textField.snp.makeConstraints { make in
+            make.top.bottom.equalToSuperview()
+            make.leading.equalTo(currencyDenominationLabel.snp.trailing).offset(16)
+            make.trailing.equalTo(textFieldContainerView.snp.trailing).offset(-16)
+
+        }
+
+    }
+}
+
+#Preview{
+    BillInputView()
+}
+
+class HeaderView: UIView {
     init() {
         super.init(frame: .zero)
         layout()
@@ -22,6 +120,4 @@ class BillInputView: UIView {
     private func layout() {
         backgroundColor = .divider
     }
-
-
 }
